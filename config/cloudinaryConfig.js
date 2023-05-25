056779ad1103b0d7c3206e974c3718804f1b02eb
naveen <solidauto202@gmail.com>
2023-05-25 14:33:46 +0530

    change made to deploy

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 require("dotenv").config()
 
 const cloudinaryConf=()=> cloudinary.config({
-    cloud_name: "dd07mszqg",
+    cloud_name:process.env.CLOUDNARY_API_NAME,
     api_key:process.env.CLOUDNARY_API_KEY,
     api_secret:process.env.CLOUDNARY_API_SECRET
 });
