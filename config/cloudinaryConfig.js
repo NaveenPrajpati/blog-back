24530d43785377b5d7891e12a055567a8abbfb01
naveen <solidauto202@gmail.com>
2023-05-25 15:09:16 +0530

    Revert "change made to deploy"
    This reverts commit 056779ad1103b0d7c3206e974c3718804f1b02eb.

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 require("dotenv").config()
 
 const cloudinaryConf=()=> cloudinary.config({
-    cloud_name:process.env.CLOUDNARY_API_NAME,
+    cloud_name: "dd07mszqg",
     api_key:process.env.CLOUDNARY_API_KEY,
     api_secret:process.env.CLOUDNARY_API_SECRET
 });
